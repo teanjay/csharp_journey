370621fdaf0fe9cc3ca47297a92f92dc1173c02a
teanjay <kgblokhin@gmail.com>
2021-09-19 21:22:33 +0300

    demo

---
+++ week1_csharp-basics/hw/1.PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N");
+            var n = Console.Read();
+            var i = 3;
+            Console.WriteLine(2);
+            while (i <= n)
+            {
+                if (i%2 != 0 && i%3 != 0 && i%5 != 0) {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
