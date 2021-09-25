afc9956e23096c6b65a665de0698a4f26d12574e
teanjay <kgblokhin@gmail.com>
2021-09-25 19:34:05 +0300

    factorial through while

---
+++ week1_csharp-basics/hw/4.Factorial/4.Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _4.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (!int.TryParse(userInput, out int a))
+                {
+                    Console.WriteLine("Invalid input");
+                    if (a < 1)
+                    {
+                        Console.WriteLine("Negative number");
+                    }
+                }
+                Console.WriteLine(factorial(a, out int result));
+            }
+        }
+        static int factorial(int a, out int result)
+        {
+            int i = 2;
+            result = 1;
+            while (i <= a)
+            {
+                result = result * i;
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
