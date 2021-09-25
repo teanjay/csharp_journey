ba4de22bc1b6562536e68eeca086af892b0d1b93
teanjay <kgblokhin@gmail.com>
2021-09-25 19:14:42 +0300

    done (with check of 0/negative)

--- week1_csharp-basics/hw/3.AreaOfTriangle/AreaOfTriangle/Program.cs
+++ week1_csharp-basics/hw/3.AreaOfTriangle/AreaOfTriangle/Program.cs
@@ -6,91 +6,60 @@ namespace AreaOfTriangle
     {
         static void Main(string[] args)
         {
-
             string userInput;
-            int a = 0, b = 0, c = 0, triangleType = 0;
             while (true)
             {
                 userInput = Console.ReadLine();
-                validateString(userInput);
-                Console.WriteLine(calculateArea(triangleType, a, b, c));
+                string[] args1 = userInput.Split();
+                if (args1.Length > 3 || args1.Length == 0)
+                {
+                    Console.WriteLine("Invalid number of arguments, try again");
+                    continue;
+                }
+                if (!tryParseIntArray(args1, out var sides))
+                {
+                    Console.WriteLine("Invalid input, try again");
+                    continue;
+                }
+                Console.WriteLine(calculateArea(sides, out var area));
 
             }
         }
-        static double calculateArea(int sidesCount, int a, int b, int c)
+        static double calculateArea(int[] sides, out double area)
         {
-            switch (sidesCount)
+            area = 0;
+            switch (sides.Length)
             {
                 case 3:
-                    a = triangleSidesArray[0];
-                    b = triangleSidesArray[1];
-                    c = triangleSidesArray[2];
+                    int a = sides[0], b = sides[1], c = sides[2];
                     double p = (a + b + c)/2;
-                    double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                    area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
                     return area;
                 case 2:
-                    a = triangleSidesArray[0];
-                    b = triangleSidesArray[1];
+                    a = sides[0];
+                    b = sides[1];
                     area = (a * b) / 2;
                     return area;
                 case 1:
-                    a = triangleSidesArray[0];
+                    a = sides[0];
                     area = ((Math.Pow(a, 2) * Math.Sqrt(3)) / 4);
                     return area;
                 default:
                     break;
             }
-            return 0;
+            return area;
         }
-        static int validateString(string userInput, out int sidesCount, out int triangleSidesArray)
+        static bool tryParseIntArray(string[] args, out int[] sides)
         {
-            var userInputArr = userInput.Split();
-            int triangleSide;
-            int triangleSidesArray = new int[userInputArr.Length];
-            if (userInputArr.Length > 2)
+            sides = new int[args.Length];
+            for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine("Invalid input, try again");
+                if (!int.TryParse(args[i], out sides[i]) || sides[i] <= 0)
+                {
+                    return false;
+                }
             }
-            else
-            {
-                for (int i = 0; i < userInputArr.Length; i++)
-                {
-                    if (int.TryParse(userInputArr[i], out triangleSide))
-                    {
-                        triangleSidesArray[i] = triangleSide;
-                    }
-
-                }
-                sidesCount = triangleSidesArray.Length;
-                if (sidesCount != userInputArr.Length)
-                {
-                    Console.WriteLine("Invalid input, try again");
-                }
-                else
-                {
-                    return sidesCount;
-                }
-                /*
-                switch (triangleType)
-                {
-                    case 3:
-                        int.TryParse(userInputArr[0], out );
-                        int.TryParse(userInputArr[1], out b);
-                        int.TryParse(userInputArr[2], out c);
-                        break;
-                    case 2:
-                        int.TryParse(userInputArr[0], out a);
-                        int.TryParse(userInputArr[1], out b);
-                        break;
-                    case 1:
-                        int.TryParse(userInputArr[0], out a);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input, try again");
-                        break;
-                }
-                */
-            }
+            return true;
         }
     }
 }
