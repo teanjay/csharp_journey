4fc1124c7f188673a1cbfe84ed5abc4feb26bbb3
Kirill Blokhin <90725378+teanjay@users.noreply.github.com>
2021-09-27 10:15:41 +0300

    beautified

--- week1_csharp-basics/hw/1.PrimeNumbers/PrimeNumbers/Program.cs
+++ week1_csharp-basics/hw/1.PrimeNumbers/PrimeNumbers/Program.cs
@@ -1,53 +1,52 @@
 using System;
 
-namespace PrimeNumbers
+namespace PrimeNumbers 
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program 
+	{
+		static void Main(string[] args) 
+		{
 			while (true) 
-			{	
-                var userInput = Console.ReadLine();
-			    if (!validateUserInput(userInput, out int n)) 
-		        	{
-			        	continue;
-		        	}
-		    	calcPrimeNumbers(n);
-             }
-        }
+			{
+				var userInput = Console.ReadLine();
+				if (!validateUserInput(userInput, out int n)) 
+				{
+					continue;
+				}
+				calcPrimeNumbers(n);
+			}
+		}
 		static bool calcPrimeNumbers(int n) 
 		{
-			 int i = 1;
-             while (i <= n)
-                 {
-					 if (i == 2 || i == 3 || i == 5 || i == 7) 
-				 	 {
-						Console.WriteLine(i);
-              		 }
-                     if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0) 
-					 {
-                  	  	Console.WriteLine(i);
-                     }
-                	 i++;
-                }
+			int i = 1;
+			while (i <= n) 
+			{
+				if (i == 2 || i == 3 || i == 5 || i == 7) 
+				{
+					Console.WriteLine(i);
+				}
+				if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) 
+				{
+					Console.WriteLine(i);
+				}
+				i++;
+			}
 			return true;
 		}
 		static bool validateUserInput(string userInput, out int n) 
 		{
-			
+
 			if (!int.TryParse(userInput, out n)) 
 			{
 				Console.WriteLine("Invalid unput");
 				return false;
 			}
-			if (n <= 1) 
-			{
+			if (n <= 1) {
 				Console.WriteLine("number is too small");
 				return false;
 			}
 			return true;
 		}
-     
+
 	}
 }
