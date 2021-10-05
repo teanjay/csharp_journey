dd9780b4061773264ef8305d7345b23179762910
teanjay <kgblokhin@gmail.com>
2021-10-05 22:08:00 +0300

    palindrome check without input

---
+++ week1_csharp-basics/hw/6. IsItPalindrome/6. IsItPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6._IsItPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "ASDFGHJKLLKJHGFDSA";
+            System.Text.StringBuilder reverse = new System.Text.StringBuilder(str);
+            bool isPalindrom = false;
+            int k = reverse.Length - 1;
+
+            for (int j = 0; j < reverse.Length/2; j++)
+            {
+                if (reverse[j] == reverse[k - j])
+                {
+                    isPalindrom = true;
+                    continue;
+                }
+                else
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPalindrom);
+        }
+    }
+}
