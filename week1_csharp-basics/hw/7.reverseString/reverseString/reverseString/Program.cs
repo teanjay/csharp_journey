750255bcd9a9c55ada6cfcf4afa051b658c6742b
teanjay <kgblokhin@gmail.com>
2021-10-09 23:48:57 +0300

    simple solution

---
+++ week1_csharp-basics/hw/7.reverseString/reverseString/reverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                char[] reversedArr = userInput.ToCharArray();
+                Array.Reverse(reversedArr);
+                Console.WriteLine(reversedArr);
+            }
+        }
+    }
+}
