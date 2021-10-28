da34a6ecc53c231eb4c956a49dbff702855d52e1
teanjay <kgblokhin@gmail.com>
2021-10-28 15:59:05 +0300

    first try wo cycles

---
+++ week1_csharp-basics/hw/5.Factorial wo cycles/5. Factorial wo cycles/5. Factorial wo cycles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Factorial_wo_cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (!validateInput(userInput, out int a))
+                {
+                    continue;
+                }
+                Console.WriteLine(factorial(a));
+            }
+        }
+        static int factorial(int a)
+        {
+            if (a == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * factorial(a - 1);
+            }
+        }
+        static bool validateInput(string userInput, out int a)
+        {
+            if (!int.TryParse(userInput, out a))
+            {
+                Console.WriteLine("Invalid input");
+                return false;
+            }
+            if (a < 1)
+            {
+                Console.WriteLine("Negative number");
+                return false;
+            }
+            return true;
+        }
+    }
+}
