bee95b04305eee1341063cef40d59de5525d1eeb
teanjay <kgblokhin@gmail.com>
2021-11-13 15:41:10 +0300

    all updates

--- week1_csharp-basics/hw/2.Discriminant/Discriminant_test/UnitTest1.cs
+++ week1_csharp-basics/hw/2.Discriminant/Discriminant_test/UnitTest1.cs
@@ -13,10 +13,7 @@ namespace Discriminant_test
         [Test]
         public void Test1()
         {
-            int a = 0;
-            int b = 0;
-            int c = 0;
-            Program.validateUserInput("1 15 29", ref a, ref b, ref c);
+            Program.getRoots("3 5 7");
             Assert.Pass();
         }
     }
